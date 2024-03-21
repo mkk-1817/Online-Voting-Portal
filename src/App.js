9fc4b83acef36c19d48e0cf20d078128bca08057
Karthik Krishna M <94902479+mkk-1817@users.noreply.github.com>
2024-03-21 09:54:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import FrontPage from './FrontPage';
+
+const App = () => {
+    return (
+        <div className="App">
+            <FrontPage />
+        </div>
+    );
+};
+
+export default App;
