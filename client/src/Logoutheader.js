5797176e43a57a52c501e1f3bb82a1e58d096f3e
Subramanian S <131273510+S-Subramanian-44@users.noreply.github.com>
2024-03-25 20:49:33 +0530

    Update Logoutheader.js

--- client/src/Logoutheader.js
+++ client/src/Logoutheader.js
@@ -8,11 +8,11 @@ export default function header(){
             <header style={{ padding: '10px 50px' }}>
                 <h2 class="OnlineVoting" style={{color: '#d2b200', fontFamily: 'Times New Roman', fontSize: '3em', marginBottom: '15px'}}>Online Voting</h2>
                 <nav class="navigation">
-                    <a href="/">Home</a>
+                    <a href="/VoterPage">Home</a>
                     <Link to="/motto">Motto</Link>
                     <Link to="/contact">Contact</Link>
                     <Link to="/about">About</Link>
-                    <Link to="/SignUp">Logout</Link>
+                    <Link to="/">Logout</Link>
                    
                 </nav>
             </header>
